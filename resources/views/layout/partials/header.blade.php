6a708605bb51c51658c84b18f3b26d1d13025ead
Donvictor <donvictorlive@gmail.com>
2026-03-23 09:51:20 +0100

    Adjust desktop header toggle spacing from wordmark

--- resources/views/layout/partials/header.blade.php
+++ resources/views/layout/partials/header.blade.php
@@ -279,14 +279,14 @@
         margin: 0 !important;
         font-size: inherit !important;
         position: absolute !important;
-        left: 250px !important;
-        top: 50% !important;
+        left: 266px !important;
+        top: calc(50% + 8px) !important;
         transform: translate(-50%, -50%) !important;
         z-index: 1041;
     }
     body.sidebar-collapsed #toggle_btn.header-toggle,
     body.mini-sidebar #toggle_btn.header-toggle {
-        left: 70px !important;
+        left: 86px !important;
     }
     .header-toggle:hover { background: #f1f5f9; color: #1e293b; }
 
