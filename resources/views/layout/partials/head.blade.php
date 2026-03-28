4400735bd652bc3adafc9e1f4630c1e126d0619a
Donvictor <donvictorlive@gmail.com>
2026-03-28 17:11:24 +0100

    Ensure Font Awesome loads with CDN fallback

--- resources/views/layout/partials/head.blade.php
+++ resources/views/layout/partials/head.blade.php
@@ -13,6 +13,7 @@
     @if (file_exists(public_path('assets/plugins/fontawesome/css/v4-shims.min.css')))
         <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome/css/v4-shims.min.css') }}">
     @endif
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" crossorigin="anonymous" referrerpolicy="no-referrer">
 @else
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/fontawesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
