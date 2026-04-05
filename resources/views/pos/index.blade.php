69283229374380f44f691541546d23914163a45d
Donvictor <donvictorlive@gmail.com>
2026-04-05 22:58:35 +0100

    Use local jquery/select2 for POS

--- resources/views/pos/index.blade.php
+++ resources/views/pos/index.blade.php
@@ -1504,8 +1504,8 @@ label {
     </div>
 </div>
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+<script src="{{ asset('assets/js/jquery-3.7.1.min.js') }}"></script>
+<script src="{{ asset('assets/js/select2.min.js') }}"></script>
 
 <script>
 $(document).ready(function() {
