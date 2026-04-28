0b0648e9038adb0018115642509dd87e02aa442c
Donvictor <donvictorlive@gmail.com>
2026-04-28 19:49:47 +0100

    Fix intercompany company add link

--- resources/views/intercompany/create.blade.php
+++ resources/views/intercompany/create.blade.php
@@ -38,12 +38,12 @@
                                     <option value="{{ $company->id }}" @selected(old('counterparty_company_id') == $company->id)>{{ $company->name ?? $company->company_name ?? ('Company #' . $company->id) }}</option>
                                 @endforeach
                             </select>
-                            <a href="{{ url('/companies/create') }}" class="btn btn-outline-primary flex-shrink-0 px-3" title="Add company" aria-label="Add company">+</a>
+                            <a href="{{ url('/superadmin/companies/create') }}" class="btn btn-outline-primary flex-shrink-0 px-3" title="Add company" aria-label="Add company">+</a>
                         </div>
                         @if($companies->isEmpty())
                             <div class="form-text text-danger">No counter-party companies are available yet. Add one to continue.</div>
                             <div class="mt-2">
-                                <a href="{{ url('/companies/create') }}" class="btn btn-sm btn-outline-primary">
+                                <a href="{{ url('/superadmin/companies/create') }}" class="btn btn-sm btn-outline-primary">
                                     <i class="fe fe-plus me-1"></i> Add Company
                                 </a>
                             </div>
