49ac0c4c3bfa7686b7a3380fad4f6f4c7a8a5010
Donvictor <donvictorlive@gmail.com>
2026-05-06 23:43:16 +0100

    remove soft deletes from grn items model

--- app/Models/GrnItem.php
+++ app/Models/GrnItem.php
@@ -3,12 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class GrnItem extends Model
 {
-    use SoftDeletes;
-
     protected $fillable = [
         'grn_id', 'product_id', 'product_name',
         'ordered_quantity', 'received_quantity', 'rejected_quantity',
